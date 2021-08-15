d0b606cdb76599369f922755ba18c9bee4bcd93d
Chaeduk <kkj2551@naver.com>
2021-08-15 13:11:46 +0900

    login layout

--- app/src/main/java/com/practice/chatting/LoginActivity.kt
+++ app/src/main/java/com/practice/chatting/LoginActivity.kt
@@ -2,10 +2,20 @@ package com.practice.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+        }
+
+        back_to_registration_text_view.setOnClickListener {
+            finish()
+        }
     }
 }
