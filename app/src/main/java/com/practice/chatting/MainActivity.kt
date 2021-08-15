0d38dbc7f30be278cb729e066740fcc1949b889d
Chaeduk <kkj2551@naver.com>
2021-08-15 16:26:40 +0900

    link with firebase

--- app/src/main/java/com/practice/chatting/MainActivity.kt
+++ app/src/main/java/com/practice/chatting/MainActivity.kt
@@ -4,21 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        auth = Firebase.auth
 
         register_button_register.setOnClickListener {
             val email = email_edittext_register.text.toString()
             val password = password_edittext_register.text.toString()
 
-            Log.d("MainActivity", "Email"+ email)
-            Log.d("MainActivity", "Password"+ password)
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Log.d("Main", "success")
+                    }
+
+                }
+
+
         }
 
         already_have_account_text_view.setOnClickListener {
