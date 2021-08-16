ebc9932addfee85d5881ed9961bb269cde63d48d
Chaeduk <kkj2551@naver.com>
2021-08-17 01:16:05 +0900

    adding

--- app/src/main/java/com/practice/chatting/NewMessageActivity.kt
+++ app/src/main/java/com/practice/chatting/NewMessageActivity.kt
@@ -2,6 +2,10 @@ package com.practice.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,24 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Select User"
 
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        recyclerview_newmessage.adapter = adapter
+
     }
+}
+
+class UserItem: Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+
 }
